d5e37a9af6aa5da3e5de933343adc71cef597d7b
vagrant <tp.bowes@gmail.com>
2019-02-12 22:24:31 +0000

    getRepoContributors implemented

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,25 @@
-var request = require('request');
+const request = require('request');
+const token = require('./secrets')
 
-function getRepoContributors(repoOwner, repoNme, cb) {
-
-}
+function getRepoContributors(repoOwner, repoName, cb) {
+    var options = {
+        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+        headers: {
+          'User-Agent': 'request',
+          'Authorization': `token ${token.GITHUB_TOKEN}`
+        }
+      };
+  
+    request(options, function(err, res, body) {
+      cb(err, JSON.parse(body));
+      });
+     
+    }
+    
 
 getRepoContributors("jquery", "jquery", function(err, result) {
     console.log("Errors:", err);
-    console.log("Result:", result);
+    result.forEach( result => {
+      console.log("Result:", result.avatar_url);
+    })
   });
